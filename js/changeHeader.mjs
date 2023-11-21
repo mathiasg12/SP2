94c5a7e44c9ae98c57f2ae092ed4dc3067acb6bb
mathiasg12 <mathiasgausl@gmail.com>
2023-11-21 14:53:02 +0100

    added profile page and styled it with bootstrap and SCSS, added spesific page

---
+++ js/changeHeader.mjs
@@ -0,0 +1,9 @@
+let token = localStorage.getItem('Token');
+let header = document.querySelector('header');
+/**
+ * trying to make a function that change the header depending if the user is logged in
+ */
+window.addEventListener('load', () => {
+  if (token == undefined) {
+  }
+});
