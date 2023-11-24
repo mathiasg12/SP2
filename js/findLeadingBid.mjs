59c15587d197b9850098fa57048eeb32cb76d9a8
mathiasg12 <mathiasgausl@gmail.com>
2023-11-24 17:11:39 +0100

    styled auction cards and added hover effect

--- js/findLeadingBid.mjs
+++ js/findLeadingBid.mjs
@@ -7,7 +7,7 @@ function getLeadingBid(bids) {
   let bidsLength = bids.length;
   let bidsIndexNr = bidsLength - 1;
   if (bidsLength >= 1) {
-    return `Current leading bid: ${bids[bidsIndexNr].amount}`;
+    return `Leading bid: ${bids[bidsIndexNr].amount} Credits`;
   } else {
     return 'No bids yet';
   }
